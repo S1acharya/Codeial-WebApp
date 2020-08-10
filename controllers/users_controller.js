a362e38b86825f9e0962ecdc8a937800bb39c8f9
S1Acharya123 <saketacharya.08@gmail.com>
2020-08-10 18:15:04 +0530

    setup partials for header and footer

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,5 @@
 module.exports.profile = function(req , res){
-    return res.render('users' , {
+    return res.render('users_profile' , {
         title: "User Profile Page!"
     });
     // res.end('<h1>User Profile</h1>');
--- index.js
+++ index.js
@@ -2,6 +2,25 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+// require layout library
+const expressLayouts = require('express-ejs-layouts');
+
+
+
+// we ask the index.js to look for static files in assets folder
+app.use(express.static('./assets'));
+
+// now we tell app to use express library
+// and this should be written before we requires ROUTES
+app.use(expressLayouts);
+
+// extract styles and scripts fro sub pages into the layout
+// for extracting css files
+app.set('layout extractStyles' , true);
+// for extracting js files
+app.set('layout extractScripts' , true);
+
+
 
 // we use express router when we have to connect home page to other pages
 // by next line , we are connecting index.js(main page) to (index.js of routes folder)
