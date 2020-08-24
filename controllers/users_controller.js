c39fd6bf1ab17d18758c21040978da476f36e1b5
S1Acharya123 <saketacharya.08@gmail.com>
2020-08-24 19:56:21 +0530

    added few styles to header and footer

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,7 +2,7 @@
 const User = require('../models/user');
 
 module.exports.profile = function(req , res){
-    return res.render('users_profile' , {
+    return res.render('user_profile' , {
         title: "User Profile Page!"
     })
     // res.end('<h1>User Profile</h1>');
